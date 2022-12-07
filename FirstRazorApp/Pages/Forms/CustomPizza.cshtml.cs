df3eb79c71b42dfe13d377cc3013fe8e76aa2287
HaikAsatryan <Haik.asatryan.95@gmail.com>
2022-12-08 02:44:38 +0400

    model creation

--- FirstRazorApp/Pages/Forms/CustomPizza.cshtml.cs
+++ FirstRazorApp/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,9 +1,13 @@
+using FirstRazorApp.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FirstRazorApp.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel Pizza { get; set; }
         public void OnGet()
         {
         }
